d80370d5f00d4ab6d3f3c8ad40d650d0586f462d
Denis Shaposhnikov <993498+dsh2dsh@users.noreply.github.com>
2025-06-29 17:55:32 +0200

    Log status code and size of responses
    Inspired by
    https://www.alexedwards.net/blog/how-to-use-the-http-responsecontroller-type
    https://github.com/urfave/negroni/blob/master/response_writer.go

---
+++ internal/http/middleware/accesslog.go
@@ -0,0 +1,124 @@
+package middleware
+
+import (
+	"io"
+	"log/slog"
+	"net/http"
+	"strings"
+	"time"
+
+	"miniflux.app/v2/internal/http/request"
+	"miniflux.app/v2/internal/logging"
+	"miniflux.app/v2/internal/storage"
+)
+
+func WithAccessLog(prefixes ...string) MiddlewareFunc {
+	m := make(map[string]struct{})
+	for _, prefix := range prefixes {
+		m[prefix] = struct{}{}
+	}
+
+	fn := func(next http.Handler) http.Handler {
+		return &AccessLog{m: m, next: next}
+	}
+	return fn
+}
+
+type AccessLog struct {
+	m    map[string]struct{}
+	next http.Handler
+}
+
+func (self *AccessLog) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	ctx := storage.WithTraceStat(r.Context())
+	sw := newStatusResponseWriter(w)
+	startTime := time.Now()
+	self.next.ServeHTTP(sw, r.WithContext(ctx))
+
+	log := logging.FromContext(ctx).With(
+		slog.String("client_ip", request.ClientIP(r)),
+		slog.String("proto", r.Proto))
+
+	if u := request.User(r); u != nil {
+		log = log.With(slog.Group("user",
+			slog.Int64("id", u.ID),
+			slog.String("name", u.Username)))
+	}
+
+	if t := storage.TraceStatFrom(ctx); t != nil && t.Queries > 0 {
+		log = log.With(slog.Group("storage",
+			slog.Int64("queries", t.Queries),
+			slog.Duration("elapsed", t.Elapsed)))
+	}
+
+	methodURL := r.Method + " " + r.URL.Path
+	log.LogAttrs(ctx, self.level(r), methodURL,
+		slog.Int("status_code", sw.StatusCode()),
+		slog.Int("size", sw.Size()),
+		slog.Duration("request_time", time.Since(startTime)))
+}
+
+func (self *AccessLog) level(r *http.Request) slog.Level {
+	p := r.URL.Path
+	if _, ok := self.m[p]; ok {
+		return slog.LevelDebug
+	}
+
+	for s := range self.m {
+		if strings.HasPrefix(p, s) {
+			return slog.LevelDebug
+		}
+	}
+	return slog.LevelInfo
+}
+
+func newStatusResponseWriter(w http.ResponseWriter) *statusResponseWriter {
+	return &statusResponseWriter{ResponseWriter: w, statusCode: http.StatusOK}
+}
+
+type statusResponseWriter struct {
+	http.ResponseWriter
+
+	statusCode    int
+	headerWritten bool
+	size          int
+}
+
+var (
+	_ io.ReaderFrom       = (*statusResponseWriter)(nil)
+	_ http.ResponseWriter = (*statusResponseWriter)(nil)
+)
+
+func (self *statusResponseWriter) StatusCode() int { return self.statusCode }
+func (self *statusResponseWriter) Size() int       { return self.size }
+
+func (self *statusResponseWriter) WriteHeader(statusCode int) {
+	self.ResponseWriter.WriteHeader(statusCode)
+	if !self.headerWritten {
+		self.statusCode = statusCode
+		self.headerWritten = true
+	}
+}
+
+func (self *statusResponseWriter) Write(b []byte) (n int, err error) {
+	self.headerWritten = true
+	n, err = self.ResponseWriter.Write(b)
+	self.size += n
+	return
+}
+
+func (self *statusResponseWriter) Unwrap() http.ResponseWriter {
+	return self.ResponseWriter
+}
+
+func (self *statusResponseWriter) ReadFrom(r io.Reader) (n int64, err error) {
+	self.headerWritten = true
+	switch v := self.ResponseWriter.(type) {
+	case io.ReaderFrom:
+		n, err = v.ReadFrom(r)
+	default:
+		n, err = io.Copy(self.ResponseWriter, r)
+	}
+	self.size += int(n)
+	return
+}
--- internal/http/middleware/middleware.go
+++ internal/http/middleware/middleware.go
@@ -4,18 +4,13 @@
 package middleware
 
 import (
-	"log/slog"
 	"net/http"
-	"strings"
-	"time"
 
 	"github.com/klauspost/compress/gzhttp"
 
 	"miniflux.app/v2/internal/config"
 	"miniflux.app/v2/internal/http/mux"
 	"miniflux.app/v2/internal/http/request"
-	"miniflux.app/v2/internal/logging"
-	"miniflux.app/v2/internal/storage"
 )
 
 type MiddlewareFunc = mux.MiddlewareFunc
@@ -37,63 +32,6 @@ func ClientIP(next http.Handler) http.Handler {
 		}
 		next.ServeHTTP(w, r.WithContext(request.WithClientIP(ctx, clientIP)))
 	})
-}
-
-func WithAccessLog(prefixes ...string) MiddlewareFunc {
-	m := make(map[string]struct{})
-	for _, prefix := range prefixes {
-		m[prefix] = struct{}{}
-	}
-
-	fn := func(next http.Handler) http.Handler {
-		return &AccessLog{m: m, next: next}
-	}
-	return fn
-}
-
-type AccessLog struct {
-	m    map[string]struct{}
-	next http.Handler
-}
-
-func (self *AccessLog) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	ctx := storage.WithTraceStat(r.Context())
-	startTime := time.Now()
-	self.next.ServeHTTP(w, r.WithContext(ctx))
-
-	log := logging.FromContext(ctx).With(
-		slog.String("client_ip", request.ClientIP(r)),
-		slog.String("proto", r.Proto))
-
-	if u := request.User(r); u != nil {
-		log = log.With(slog.Group("user",
-			slog.Int64("id", u.ID),
-			slog.String("name", u.Username)))
-	}
-
-	if t := storage.TraceStatFrom(ctx); t != nil && t.Queries > 0 {
-		log = log.With(slog.Group("storage",
-			slog.Int64("queries", t.Queries),
-			slog.Duration("elapsed", t.Elapsed)))
-	}
-
-	methodURL := r.Method + " " + r.URL.Path
-	log.LogAttrs(ctx, self.level(r), methodURL,
-		slog.Duration("request_time", time.Since(startTime)))
-}
-
-func (self *AccessLog) level(r *http.Request) slog.Level {
-	p := r.URL.Path
-	if _, ok := self.m[p]; ok {
-		return slog.LevelDebug
-	}
-
-	for s := range self.m {
-		if strings.HasPrefix(p, s) {
-			return slog.LevelDebug
-		}
-	}
-	return slog.LevelInfo
 }
 
 func Gzip(next http.Handler) http.Handler {
