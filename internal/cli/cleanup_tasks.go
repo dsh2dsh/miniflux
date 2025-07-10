8092980495d6c1b4795998eed0ae9ac3be8d43ca
Denis Shaposhnikov <993498+dsh2dsh@users.noreply.github.com>
2025-07-10 20:42:04 +0200

    Use context logger for cleanup tasks

--- internal/cli/cleanup_tasks.go
+++ internal/cli/cleanup_tasks.go
@@ -11,6 +11,7 @@ import (
 	"github.com/spf13/cobra"
 
 	"miniflux.app/v2/internal/config"
+	"miniflux.app/v2/internal/logging"
 	"miniflux.app/v2/internal/metric"
 	"miniflux.app/v2/internal/model"
 	"miniflux.app/v2/internal/storage"
@@ -34,18 +35,19 @@ func runCleanupTasks(ctx context.Context, store *storage.Storage) {
 	removed := store.CleanOldSessions(ctx,
 		config.Opts.CleanupRemoveSessionsDays(),
 		config.Opts.CleanupInactiveSessionsDays())
-	slog.Info("Sessions cleanup completed",
-		slog.Int64("removed", removed))
+
+	log := logging.FromContext(ctx)
+	log.Info("Sessions cleanup completed", slog.Int64("removed", removed))
 
 	startTime := time.Now()
-	rowsAffected, err := store.ArchiveEntries(ctx, model.EntryStatusRead,
+	rows, err := store.ArchiveEntries(ctx, model.EntryStatusRead,
 		config.Opts.CleanupArchiveReadDays(),
 		config.Opts.CleanupArchiveBatchSize())
 	if err != nil {
-		slog.Error("Unable to archive read entries", slog.Any("error", err))
+		log.Error("Unable to archive read entries", slog.Any("error", err))
 	} else {
-		slog.Info("Archiving read entries completed",
-			slog.Int64("read_entries_archived", rowsAffected),
+		log.Info("Archiving read entries completed",
+			slog.Int64("read_entries_archived", rows),
 			slog.Duration("elapsed", time.Since(startTime)))
 
 		if config.Opts.HasMetricsCollector() {
@@ -56,14 +58,14 @@ func runCleanupTasks(ctx context.Context, store *storage.Storage) {
 	}
 
 	startTime = time.Now()
-	rowsAffected, err = store.ArchiveEntries(ctx, model.EntryStatusUnread,
+	rows, err = store.ArchiveEntries(ctx, model.EntryStatusUnread,
 		config.Opts.CleanupArchiveUnreadDays(),
 		config.Opts.CleanupArchiveBatchSize())
 	if err != nil {
-		slog.Error("Unable to archive unread entries", slog.Any("error", err))
+		log.Error("Unable to archive unread entries", slog.Any("error", err))
 	} else {
-		slog.Info("Archiving unread entries completed",
-			slog.Int64("unread_entries_archived", rowsAffected),
+		log.Info("Archiving unread entries completed",
+			slog.Int64("unread_entries_archived", rows),
 			slog.Duration("elapsed", time.Since(startTime)))
 
 		if config.Opts.HasMetricsCollector() {
