fd188d4c532f71e4afb2835429ef1a7c7dbacf61
Denis Shaposhnikov <993498+dsh2dsh@users.noreply.github.com>
2025-08-23 23:17:06 +0200

    Show Loading... during entry content inlining

--- internal/ui/static/js/inline_entry.js
+++ internal/ui/static/js/inline_entry.js
@@ -1,7 +1,19 @@
 class InlineEntry {
   constructor() {
+    this.listenBeforeSend();
     this.listenAfterSettle();
     this.listenLinkClick();
+  }
+
+  listenBeforeSend() {
+    document.body.addEventListener("htmx:beforeSend", (event) => {
+      const target = event.detail.target;
+      if (target.matches(".item-header")) {
+        this.loadingInline(target);
+      } else if (target.matches(".entry-content.inline")) {
+        this.downloading(event.target, target);
+      }
+    });
   }
 
   listenAfterSettle() {
@@ -27,6 +39,15 @@ class InlineEntry {
     });
   }
 
+  loadingInline(el) {
+    const t = document.querySelector("template#loading-indicator");
+    el.after(t.content.cloneNode(true));
+    el.addEventListener("htmx:beforeSwap", () => {
+      const item = el.parentElement;
+      item.querySelector(".htmx-indicator").remove();
+    }, { once: true });
+  }
+
   entryInlined(el) {
     const titleLink = el.querySelector(".item-title a");
     titleLink.dataset.hxDisable = "true";
@@ -38,6 +59,24 @@ class InlineEntry {
 
   markItemRead(el) {
     markItemsRead([el]);
+  }
+
+  downloading(button, target) {
+    this.setButtonLoading(button);
+    const item = target.closest(".item");
+    item.addEventListener("htmx:afterSettle", (event) => {
+      if (event.detail.elt.matches(".entry-content.download")) {
+        button.parentElement.remove();
+      }
+    }, { once: true });
+  }
+
+  setButtonLoading(button) {
+    const originalLabel = button.querySelector(".icon-label")
+    const loadingLabel = createIconLabelElement(
+      document.body.dataset.labelLoading);
+    button.replaceChild(loadingLabel, originalLabel);
+    return originalLabel;
   }
 
   downloaded(el) {
