5ea8b1df4ec4d8f207212f44aa347b4808601dba
Denis Shaposhnikov <993498+dsh2dsh@users.noreply.github.com>
2025-09-24 17:52:36 +0200

    Move (*ResponseHandler).LocalizedError to errors.go

--- internal/reader/fetcher/response_handler.go
+++ internal/reader/fetcher/response_handler.go
@@ -5,16 +5,13 @@ package fetcher // import "miniflux.app/v2/internal/reader/fetcher"
 
 import (
 	"bytes"
-	"crypto/x509"
 	"errors"
 	"fmt"
 	"io"
 	"log/slog"
 	"math"
-	"net"
 	"net/http"
 	"net/url"
-	"os"
 	"strconv"
 	"strings"
 	"time"
@@ -199,105 +196,4 @@ func (r *ResponseHandler) ReadBody() ([]byte, *locale.LocalizedErrorWrapper) {
 			"error.http_empty_response_body")
 	}
 	return buffer.Bytes(), nil
-}
-
-func (r *ResponseHandler) LocalizedError() *locale.LocalizedErrorWrapper {
-	if r.clientErr != nil {
-		switch {
-		case isSSLError(r.clientErr):
-			return locale.NewLocalizedErrorWrapper(fmt.Errorf("fetcher: %w", r.clientErr), "error.tls_error", r.clientErr)
-		case isNetworkError(r.clientErr):
-			return locale.NewLocalizedErrorWrapper(fmt.Errorf("fetcher: %w", r.clientErr), "error.network_operation", r.clientErr)
-		case os.IsTimeout(r.clientErr):
-			return locale.NewLocalizedErrorWrapper(fmt.Errorf("fetcher: %w", r.clientErr), "error.network_timeout", r.clientErr)
-		case errors.Is(r.clientErr, io.EOF):
-			return locale.NewLocalizedErrorWrapper(fmt.Errorf("fetcher: %w", r.clientErr), "error.http_empty_response")
-		default:
-			return locale.NewLocalizedErrorWrapper(fmt.Errorf("fetcher: %w", r.clientErr), "error.http_client_error", r.clientErr)
-		}
-	}
-
-	switch r.httpResponse.StatusCode {
-	case http.StatusUnauthorized:
-		return locale.NewLocalizedErrorWrapper(
-			errors.New("fetcher: access unauthorized (401 status code)"),
-			"error.http_not_authorized")
-	case http.StatusForbidden:
-		return locale.NewLocalizedErrorWrapper(
-			errors.New("fetcher: access forbidden (403 status code)"),
-			"error.http_forbidden")
-	case http.StatusTooManyRequests:
-		return locale.NewLocalizedErrorWrapper(
-			errors.New("fetcher: too many requests (429 status code)"),
-			"error.http_too_many_requests")
-	case http.StatusNotFound, http.StatusGone:
-		return locale.NewLocalizedErrorWrapper(
-			fmt.Errorf("fetcher: resource not found (%d status code)",
-				r.httpResponse.StatusCode),
-			"error.http_resource_not_found")
-	case http.StatusInternalServerError:
-		return locale.NewLocalizedErrorWrapper(
-			fmt.Errorf("fetcher: remote server error (%d status code)",
-				r.httpResponse.StatusCode),
-			"error.http_internal_server_error")
-	case http.StatusBadGateway:
-		return locale.NewLocalizedErrorWrapper(
-			fmt.Errorf("fetcher: bad gateway (%d status code)",
-				r.httpResponse.StatusCode),
-			"error.http_bad_gateway")
-	case http.StatusServiceUnavailable:
-		return locale.NewLocalizedErrorWrapper(
-			fmt.Errorf("fetcher: service unavailable (%d status code)",
-				r.httpResponse.StatusCode),
-			"error.http_service_unavailable")
-	case http.StatusGatewayTimeout:
-		return locale.NewLocalizedErrorWrapper(
-			fmt.Errorf("fetcher: gateway timeout (%d status code)",
-				r.httpResponse.StatusCode),
-			"error.http_gateway_timeout")
-	}
-
-	if r.httpResponse.StatusCode >= 400 {
-		return locale.NewLocalizedErrorWrapper(
-			fmt.Errorf("fetcher: unexpected status code (%d status code)",
-				r.httpResponse.StatusCode),
-			"error.http_unexpected_status_code", r.httpResponse.StatusCode)
-	}
-
-	if r.httpResponse.StatusCode != http.StatusNotModified {
-		// Content-Length = -1 when no Content-Length header is sent.
-		if r.httpResponse.ContentLength == 0 {
-			return locale.NewLocalizedErrorWrapper(
-				errors.New("fetcher: empty response body"),
-				"error.http_empty_response_body")
-		}
-	}
-	return nil
-}
-
-func isNetworkError(err error) bool {
-	var urlErr *url.Error
-	if errors.As(err, &urlErr) {
-		return true
-	}
-	if errors.Is(err, io.EOF) {
-		return true
-	}
-	var opErr *net.OpError
-	return errors.As(err, &opErr)
-}
-
-func isSSLError(err error) bool {
-	var certErr x509.UnknownAuthorityError
-	if errors.As(err, &certErr) {
-		return true
-	}
-
-	var hostErr x509.HostnameError
-	if errors.As(err, &hostErr) {
-		return true
-	}
-
-	var algErr x509.InsecureAlgorithmError
-	return errors.As(err, &algErr)
 }
