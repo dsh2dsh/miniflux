e9997b38b0d5ec14ec79a5862e6c9fecce5c3265
Denis Shaposhnikov <993498+dsh2dsh@users.noreply.github.com>
2026-02-11 19:37:29 +0100

    Reuse slog.MultiHandler

--- internal/cli/logger/multi.go
+++ internal/cli/logger/multi.go
@@ -9,12 +9,18 @@ import (
 )
 
 func NewMultiHandler(handlers []slog.Handler) *MultiHandler {
-	return &MultiHandler{handlers: handlers}
+	self := &MultiHandler{
+		MultiHandler: slog.NewMultiHandler(handlers...),
+		h0:           handlers[0],
+	}
+	return self
 }
 
 type MultiHandler struct {
-	closers  []io.Closer
-	handlers []slog.Handler
+	*slog.MultiHandler
+
+	h0      slog.Handler
+	closers []io.Closer
 }
 
 var _ slog.Handler = (*MultiHandler)(nil)
@@ -24,53 +30,26 @@ func (self *MultiHandler) WithClosers(closers []io.Closer) *MultiHandler {
 	return self
 }
 
-func (self *MultiHandler) Enabled(ctx context.Context, level slog.Level) bool {
-	for _, h := range self.handlers {
-		if h.Enabled(ctx, level) {
-			return true
-		}
-	}
-	return false
-}
-
 func (self *MultiHandler) Handle(ctx context.Context, r slog.Record) error {
-	for i, h := range self.handlers {
-		if !h.Enabled(ctx, r.Level) {
-			continue
-		}
-		if err := h.Handle(ctx, r.Clone()); err != nil {
-			err = fmt.Errorf("logger: handler #%v failed: %w", i, err)
-			self.logInternalErr(ctx, i, err)
-			return err
-		}
+	err := self.MultiHandler.Handle(ctx, r)
+	if err == nil {
+		return nil
 	}
-	return nil
+
+	err = fmt.Errorf("logger: one of handlers failed: %w", err)
+	self.logInternalErr(ctx, err)
+	return err
 }
 
-func (self *MultiHandler) logInternalErr(ctx context.Context, i int, err error) {
-	if i == 0 || !self.handlers[0].Enabled(ctx, slog.LevelError) {
+func (self *MultiHandler) logInternalErr(ctx context.Context, err error) {
+	if !self.h0.Enabled(ctx, slog.LevelError) {
 		return
 	}
-	r := slog.NewRecord(time.Now(), slog.LevelError, "log handler failed",
+
+	r := slog.NewRecord(time.Now(), slog.LevelError, "unable log message",
 		uintptr(0))
 	r.AddAttrs(slog.Any("error", err))
-	_ = self.handlers[0].Handle(ctx, r)
-}
-
-func (self *MultiHandler) WithAttrs(attrs []slog.Attr) slog.Handler {
-	handlers := make([]slog.Handler, len(self.handlers))
-	for i := range self.handlers {
-		handlers[i] = self.handlers[i].WithAttrs(attrs)
-	}
-	return &MultiHandler{handlers: handlers}
-}
-
-func (self *MultiHandler) WithGroup(name string) slog.Handler {
-	handlers := make([]slog.Handler, len(self.handlers))
-	for i := range self.handlers {
-		handlers[i] = self.handlers[i].WithGroup(name)
-	}
-	return &MultiHandler{handlers: handlers}
+	_ = self.h0.Handle(ctx, r)
 }
 
 func (self *MultiHandler) Close() error {
